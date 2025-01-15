408113e5d26cb0155770e388bb74d17972ee5837
arunav321 <84830602+arunav321@users.noreply.github.com>
2025-01-15 10:33:26 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -25,6 +25,6 @@ module tt_um_haahalya (
     assign uo_out[7:2]=6'b0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
